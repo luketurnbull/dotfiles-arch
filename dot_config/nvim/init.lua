7696ea8d090f48c729756bd2ca62ab4c2d4d8415
Luke Turnbull <luke.turn.dev@gmail.com>
2026-06-06 17:26:44 +1000

    Added several basic keymaps

--- dot_config/nvim/init.lua
+++ dot_config/nvim/init.lua
@@ -1,18 +1,7 @@
 require("vim._core.ui2").enable({})
 
 require('options')
-
--- Search
-vim.o.ignorecase = true -- Ignore the case when searching...
-vim.o.smartcase = true -- ...unless the query specifically has an uppercase letter
-
--- Tabs and spaces
-vim.o.expandtab = true -- Tabs become spaces
-vim.o.shiftwidth = 2 -- Indent width
-vim.o.tabstop = 2 -- Tab renders as 2 spaces
-vim.o.signcolumn = "yes" -- Always show the sign column
-vim.o.scrolloff = 8 -- Keep 8 lines of context above/below the cursor
-vim.o.termguicolors = true -- 24-bit colour (Needed by modern terminals)
+require('keymaps')
 
 -- Packages
 -- Tokyo Night theme, add it as a package and set it
---
+++ dot_config/nvim/lua/empty_keymaps.lua
@@ -0,0 +1,31 @@
+vim.g.mapleader = " "
+
+-- replaces select test WITHOUT losing what you yanked
+vim.keymap.set("x", "p", [["_dP]], { desc = "Paste over selection without losing yanked text" })
+
+-- Delete text without saving it to any register
+vim.keymap.set({ "n", "v" }, "<leader>d", [["_d]], { desc = "Delete without yanking" })
+
+-- Clear search highlighing
+vim.keymap.set("n", "<C-c>", ":nohl<CR>", { desc = "Clear search highlighting", silent = true })
+
+-- Move lines up and down
+vim.keymap.set("v", "J", ":m '>+1<CR>gv=gv", { desc = "moves lines down in visual selection" })
+vim.keymap.set("v", "K", ":m '<-2<CR>gv=gv", { desc = "moves lines up in visual selection" })
+
+-- Indent selected lines
+vim.keymap.set("v", "<", "<gv", { desc = "Unindent and keep selection" })
+vim.keymap.set("v", ">", ">gv", { desc = "Indent and keep selection" })
+
+-- Keep cursor when scrolling up and down
+vim.keymap.set("n", "<C-d>", "<C-d>zz", { desc = "Move down with cursor centered" })
+vim.keymap.set("n", "<C-u>", "<C-u>zz", { desc = "Move up with cursor centered" })
+
+-- Replace word cursor is over and all instances of that word within the current buffer
+vim.keymap.set("n", "<leader>s", [[:%s/\<<C-r><C-w>\>/<C-r><C-w>/gI<Left><Left><Left>>]], { desc = "Replace word cursor is over" })
+
+-- Native undotree
+vim.keymap.set("n", "<leader>u", function()
+  vim.cmd.packadd("nvim.undotree")
+  require("undotree").open()
+end, { desc = "Toggle Builtin Undotree" })
