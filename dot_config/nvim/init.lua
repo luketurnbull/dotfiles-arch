bbea2f70ccc501d81c64fab0d680953b4715c690
Luke Turnbull <luke.turn.dev@gmail.com>
2026-06-10 17:41:39 +1000

    Added .luarc.json for nvim in chezmoi also added better lsp support and move lua lsp to it's own file in the lsp folder

--- dot_config/nvim/init.lua
+++ dot_config/nvim/init.lua
@@ -7,14 +7,23 @@ require('pack')
 vim.cmd.colorscheme("tokyonight-night")
 
 -- LSP
--- Lua LSP
-vim.lsp.config('lua_ls', {
-  cmd = { 'lua-language-server' },
-  filetypes = { 'lua' },
-  root_markers = { '.luarc.json', '.git' },
-  settings = {
-    Lua = { diagnostics = { globals = { 'vim' }}},
-  },
-})
 vim.lsp.enable('lua_ls')
 
+vim.api.nvim_create_autocmd("LspAttach", {
+  callback = function(args)
+    local client = vim.lsp.get_client_by_id(args.data.client_id)
+    local opts = { buffer = args.buf }
+
+    -- navigation
+    vim.keymap.set('n', 'gd', vim.lsp.buf.definition, opts)
+    vim.keymap.set('n', 'gD', vim.lsp.buf.declaration, opts)
+
+    -- intellisense: autocomplete as you type (native, no plugin)
+    if client and client:supports_method("textDocument/completion") then
+      vim.lsp.completion.enable(true, client.id, args.buf, { autotrigger = true })
+    end
+  end,
+})
+
+-- popup behaviour: show menu even for one match, don't auto-insert, fuzzy match
+vim.o.completeopt = 'menu,menuone,noinsert,fuzzy'
