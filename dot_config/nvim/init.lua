fd119b68b4c2dcf499960f890409b5e3cfcf7bb2
Luke Turnbull <luke.turn.dev@gmail.com>
2026-06-11 14:40:44 +1000

    Add stylelua and coform for format on save

--- dot_config/nvim/init.lua
+++ dot_config/nvim/init.lua
@@ -8,28 +8,33 @@ vim.cmd.colorscheme("tokyonight-night")
 
 -- LSP
 require("blink.cmp").setup({
-  keymap = {
-    preset = "default",
-    ["<Right>"] = { "select_and_accept", "fallback" },
-  },
-  apperance = { nerd_font_variable = "mono" },
-  sources = { default = { "lsp", "path", "snippets", "buffer" } },
-  fuzzy = { implementation = "prefer_rust_with_warning" },
-  signature = { enabled = true },
-  completion = {
-    documentation = { auto_show = true, auto_show_delay_ms = 200 },
-    ghost_text = { enabled = true },
-  },
+    keymap = {
+        preset = "default",
+        ["<Right>"] = { "select_and_accept", "fallback" },
+    },
+    apperance = { nerd_font_variable = "mono" },
+    sources = { default = { "lsp", "path", "snippets", "buffer" } },
+    fuzzy = { implementation = "prefer_rust_with_warning" },
+    signature = { enabled = true },
+    completion = {
+        documentation = { auto_show = true, auto_show_delay_ms = 200 },
+        ghost_text = { enabled = true },
+    },
+})
+
+require("conform").setup({
+    formatters_by_ft = { lua = { "stylelua" } },
+    format_on_save = { timeout_ms = 500, lsp_format = "fallback" },
 })
 
 vim.lsp.enable('lua_ls')
 
 vim.api.nvim_create_autocmd("LspAttach", {
-  callback = function(args)
-    local opts = { buffer = args.buf }
+    callback = function(args)
+        local opts = { buffer = args.buf }
 
-    -- navigation
-    vim.keymap.set('n', 'gd', vim.lsp.buf.definition, opts)
-    vim.keymap.set('n', 'gD', vim.lsp.buf.declaration, opts)
-  end,
+        -- navigation
+        vim.keymap.set('n', 'gd', vim.lsp.buf.definition, opts)
+        vim.keymap.set('n', 'gD', vim.lsp.buf.declaration, opts)
+    end,
 })
--- dot_config/nvim/lua/empty_pack.lua
+++ dot_config/nvim/lua/empty_pack.lua
@@ -2,4 +2,5 @@ vim.pack.add({
   { src = "https://github.com/folke/tokyonight.nvim" },
   { src = "https://github.com/saghen/blink.cmp", version = vim.version.range("^1") },
   { src = "https://github.com/rafamadriz/friendly-snippets" },
+  { src = "https://github.com/stevearc/conform.nvim" },
 })
