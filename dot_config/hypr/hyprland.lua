eadf000c2e225c27fa5a63a3c3cee2ea804a5dd4
Luke Turnbull <luke.turn.dev@gmail.com>
2026-06-13 13:52:12 +1000

    Added workspaces 1 to 10 to hypeland config

--- dot_config/hypr/hyprland.lua
+++ dot_config/hypr/hyprland.lua
@@ -61,6 +61,14 @@ hl.bind(mainMod .. " + SHIFT + down", hl.dsp.window.move({ direction = "down" })
 hl.bind(mainMod .. " + SHIFT + up", hl.dsp.window.move({ direction = "up" }))
 hl.bind(mainMod .. " + SHIFT + right", hl.dsp.window.move({ direction = "right" }))
 
+-- Switch workspaces with mainMod + [0-9]
+-- Move active window to a workspace with mainMod + SHIFT + [0-9]
+for i = 1, 10 do
+    local key = i % 10 -- 10 maps to key 0
+    hl.bind(mainMod .. " + " .. key, hl.dsp.focus({ workspace = i }))
+    hl.bind(mainMod .. " + SHIFT + " .. key, hl.dsp.window.move({ workspace = i }))
+end
+
 -- Quit Hyprland
 hl.bind(mainMod .. " + M", hl.dsp.exit())
 
