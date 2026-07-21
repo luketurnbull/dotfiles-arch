eed3845e4a64f748a8592c731b5a80dc714943d4
Luke Turnbull <luke.turn.dev@gmail.com>
2026-07-21 17:37:45 +1000

    Added keymaps to allow movement of lines in visual mode and to center the cursor when doing half page scrolls

--- dot_config/nvim/lua/mappings.lua
+++ dot_config/nvim/lua/mappings.lua
@@ -1,10 +1,19 @@
-require "nvchad.mappings"
+require("nvchad.mappings")
 
 -- add yours here
 
 local map = vim.keymap.set
 
-map("n", ";", ":", { desc = "CMD enter command mode" })
-map("i", "jk", "<ESC>")
+-- Visual mode: move selected lines up/down
+map("v", "J", ":m '>+1<CR>gv=gv", { desc = "move line down" })
+map("v", "K", ":m '<-2<CR>gv=gv", { desc = "move line up" })
 
--- map({ "n", "i", "v" }, "<C-s>", "<cmd> w <cr>")
+-- Center cursor after half-page scrolls
+map("n", "<C-d>", "<C-d>zz", {
+	desc = "scroll down and center",
+})
+map("n", "<C-u>", "<C-u>zz", {
+	desc = "scroll up and center",
+})
+map("n", "<C-o>", "<C-o>zz", { desc = "jump back and center" })
+map("n", "<C-i>", "<C-i>zz", { desc = "jump forward and center" })
